41474c0830c2f982baf06f8e2742f0862199b309
Carie C Navio <ccnavio@alaska.edu>
2017-09-14 17:35:07 -0800

    Final versions for HW1

--- HW1/product.cpp
+++ HW1/product.cpp
@@ -1,6 +1,9 @@
 // product.cpp
 // Carie Navio
 // 6 Sep 2017
+// 
+// CS 311 Fall 2017
+// Homework 1 
 
 #include "product.h"
 #include <string>
@@ -10,23 +13,36 @@ using std::ostream;
 #include <sstream>
 using std::ostringstream;
 
-string Product::toString() const {
+// Definition of member functions
+//
+// Product::toString()
+// See header for documentation
+string Product::toString() const 
+{
 	string name = getName();
 	int sales = getSales();
 
 	ostringstream ostr;
-	ostr << name << "(sales: "<< sales <<")";
+	ostr << name << " (sales: "<< sales <<")";
 	return ostr.str();
 }
 
+// Global operators for class Product
+
+// op == (Product, Product)
+// See header for documentation
 bool operator==(const Product & a, const Product & b){
 	return (a.getName() == b.getName()) && (a.getSales() == b.getSales());
 }
 
+// op != (Product, Product)
+// See header for documentation
 bool operator!=(const Product & a, const Product & b){
 	return !(a == b);
 }
 
+// op<<(ostream, Product)
+// See header for documentation
 ostream & operator<<(ostream & out, const Product & p){
 	out << p.toString();
 	return out;
--- HW1/product.h
+++ HW1/product.h
@@ -1,65 +1,97 @@
 // product.h
 // Carie Navio
 // 6 Sep 2017
+// 
+// CS 311 Fall 2017
+// Homework 1 
 
-#ifndef PRODUCT_H
-#define PRODUCT_H
+#ifndef FILE_PRODUCT_H_INCLUDED
+#define FILE_PRODUCT_H_INCLUDED 
 
 #include <ostream>
 using std::ostream;
 #include <string>
 
+// Class Product - Class identification
+// 
+// Class Product
+// Product names, number of sales
 
 class Product {
 
+// Product: ctors, dtors, assignements
 public:
 	// Default ctor
+	// Sets name to empty string and number of sales to 0
+	// Pre: None
+	// Post: Empty string as name, zero sales
 	Product()
 	{
 		setName("");
 		setSales(0);
 	}
 
+	// Ctor from data
+	// Sets names and sales to passed parameters
+	// Pre : Sales >= 0
+	//Post: Name and sales set to passed parameters
 	Product(std::string name, int sales)
 	{
 		setName(name);
 		setSales(sales);
 	}
 
+	// Use compiler generated verstions for the five 
 	~Product() = default;
 	Product(const Product & other) = default;
 	Product(Product && other) = default;
 	Product & operator=(const Product & other) = default;
 	Product & operator=(Product && other) = default;
 
+// General member functions
 public:
-
+	// getName
+	// Pre: None
+	// Post: Return is name of the product represented by *this
 	std::string getName() const {
 		return name_;
 	}
 
+	// getSales
+	// Pre: None
+	// Post: Return is number of the sales represented by *this
 	int getSales() const {
 		return sales_;
 	}
 
+	// setName
+	// Pre: None
+	// Post: Product name is set to passed string
 	void setName(std::string new_name) {
 		name_ = new_name;
 	}
 
+	// setSales
+	// Pre: None
+	// Post: Product sale is set to passed int
 	void setSales(int new_sales) {
 		sales_ = new_sales;
 	}
 
-	std::string toString() const;
-
+// Public operators 
 public:
 
+	// op++ [pre]
+	// Pre: None
+	// Post: Product sales incremented by 1, state of *this returned
 	Product & operator++()
 	{
 		++sales_;
 		return *this;
 	}
-
+	// op++ [post]
+	// Pre: None
+	// Post: Product sales incremented by 1, former state of *this returned 
 	Product operator++(int dummy)
 	{
 		Product save = *this;
@@ -67,6 +99,10 @@ public:
 		return save;
 	}
 
+	// op-- [pre]
+	// Pre: None
+	// Post: Product sales decremented by 1, state of *this returned
+	// Must be greater than 0
 	Product & operator--()
 	{
 		if (sales_ > 0)
@@ -74,6 +110,10 @@ public:
 		return *this;
 	}
 
+	// op-- [post]
+	// Pre: None
+	// Post: Product sales decremented by 1, former state of *this returned 
+	// Must bee greater than 0
 	Product operator--(int dummy)
 	{
 		Product save = *this;
@@ -81,14 +121,35 @@ public:
 		return save;
 	}
 
+	// toString
+	// Return string of Product data members
+	// Pre: None
+	// Post: Return is in string format
+	// Defined in source file 
+	std::string toString() const;
+
+// Data members
 private:
 	std::string name_;
 	int sales_;
 };
+// End of class Product
 
+// op==(Product, Product)
+// Pre: None
+// Post: Return true if a==b
+// (both names and sales are equal)
 bool operator==(const Product & a, const Product & b);
+
+// op!=(Product, Product)
+// Pre: None
+// Post: Return true if a != b
+// (either the name of sales are not equal)
 bool operator!=(const Product & a, const Product & b);
 
+// op<<(ostream, Product)
+// Pre: None
+// Post: Output the given Product in the same form as toString
 ostream & operator<<(ostream & out, const Product & p);
 
-#endif
+#endif //#ifndef FILE_PRODUCT_H_INCLUDED
